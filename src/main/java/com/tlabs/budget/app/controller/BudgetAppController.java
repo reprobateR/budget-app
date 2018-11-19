d67494dea3ea5742cf8127d4cd3a1add4c4593d8
Raghav Tenneti <Raghav Tenneti@DESKTOP-VF1S1LE>
2018-11-19 17:55:46 +0530

    push local

--- src/main/java/com/tlabs/budget/app/controller/BudgetAppController.java
+++ src/main/java/com/tlabs/budget/app/controller/BudgetAppController.java
@@ -1,5 +1,7 @@
 package com.tlabs.budget.app.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,11 +13,13 @@ import com.tlabs.budget.app.model.BudgetSheet;
 @RestController
 @RequestMapping("/budget")
 public class BudgetAppController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(BudgetAppController.class);
 
 	@PostMapping("/save")
 	public ResponseEntity<String> getTransactions(@RequestBody BudgetSheet budgetSheet){
 
-		System.out.println("save budget hit " + budgetSheet);
+		LOGGER.info(("save budget hit " + budgetSheet));
 
 		return ResponseEntity.ok().body("Success!!");
     }
