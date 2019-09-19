ce7a82906db6c8218140da8986ee3c0e84748db8
reprobateR <40433067+reprobateR@users.noreply.github.com>
2019-09-19 15:11:08 +0530

    Add files via upload

---
+++ budget.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $("#download-btn").click(function () {
+        alert("Download Button Clicked!")
+    });
+
+    $("#log-in").click(function () {
+        alert("Download Button Clicked!")
+    });
+
+});
