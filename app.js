d0dec61f9cf4a7d2fb96ed14999fb929bcf1a03a
reprobateR <40433067+reprobateR@users.noreply.github.com>
2019-09-22 16:24:47 +0530

    Saving Transactions

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var budgetController = (function() {
         this.percentage = -1;
     };
     
-    
     Expense.prototype.calcPercentage = function(totalIncome) {
         if (totalIncome > 0) {
             this.percentage = Math.round((this.value / totalIncome) * 100);
@@ -17,19 +16,16 @@ var budgetController = (function() {
         }
     };
     
-    
     Expense.prototype.getPercentage = function() {
         return this.percentage;
     };
-    
     
     var Income = function(id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
     };
-    
-    
+   
     var calculateTotal = function(type) {
         var sum = 0;
         data.allItems[type].forEach(function(cur) {
@@ -37,7 +33,6 @@ var budgetController = (function() {
         });
         data.totals[type] = sum;
     };
-    
     
     var data = {
         allItems: {
@@ -51,90 +46,49 @@ var budgetController = (function() {
         budget: 0,
         percentage: -1
     };
-    
-    
+   
     return {
         addItem: function(type, des, val) {
             var newItem, ID;
-            
-            //[1 2 3 4 5], next ID = 6
-            //[1 2 4 6 8], next ID = 9
-            // ID = last ID + 1
-            
-            // Create new ID
             if (data.allItems[type].length > 0) {
                 ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
             } else {
                 ID = 0;
             }
-            
-            // Create new item based on 'inc' or 'exp' type
             if (type === 'exp') {
                 newItem = new Expense(ID, des, val);
             } else if (type === 'inc') {
                 newItem = new Income(ID, des, val);
             }
-            
-            // Push it into our data structure
             data.allItems[type].push(newItem);
-            
-            // Return the new element
             return newItem;
         },
         
-        
         deleteItem: function(type, id) {
             var ids, index;
-            
-            // id = 6
-            //data.allItems[type][id];
-            // ids = [1 2 4  8]
-            //index = 3
-            
             ids = data.allItems[type].map(function(current) {
                 return current.id;
             });
-
             index = ids.indexOf(id);
-
             if (index !== -1) {
                 data.allItems[type].splice(index, 1);
             }
-            
         },
         
         
         calculateBudget: function() {
-            
-            // calculate total income and expenses
             calculateTotal('exp');
             calculateTotal('inc');
+             data.budget = data.totals.inc - data.totals.exp;
             
-            // Calculate the budget: income - expenses
-            data.budget = data.totals.inc - data.totals.exp;
-            
-            // calculate the percentage of income that we spent
             if (data.totals.inc > 0) {
                 data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
             } else {
                 data.percentage = -1;
             }            
-            
-            // Expense = 100 and income 300, spent 33.333% = 100/300 = 0.3333 * 100
         },
         
         calculatePercentages: function() {
-            
-            /*
-            a=20
-            b=10
-            c=40
-            income = 100
-            a=20/100=20%
-            b=10/100=10%
-            c=40/100=40%
-            */
-            
             data.allItems.exp.forEach(function(cur) {
                cur.calcPercentage(data.totals.inc);
             });
@@ -147,8 +101,6 @@ var budgetController = (function() {
             });
             return allPerc;
         },
-        
-        
         getBudget: function() {
             return {
                 budget: data.budget,
@@ -190,15 +142,6 @@ var UIController = (function() {
     
     var formatNumber = function(num, type) {
         var numSplit, int, dec, type;
-        /*
-            + or - before number
-            exactly 2 decimal points
-            comma separating the thousands
-
-            2310.4567 -> + 2,310.46
-            2000 -> + 2,000.00
-            */
-
         num = Math.abs(num);
         num = num.toFixed(2);
 
@@ -206,13 +149,10 @@ var UIController = (function() {
 
         int = numSplit[0];
         if (int.length > 3) {
-            int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3); //input 23510, output 23,510
+            int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3);
         }
-
         dec = numSplit[1];
-
         return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
-
     };
     
     
@@ -226,7 +166,7 @@ var UIController = (function() {
     return {
         getInput: function() {
             return {
-                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                type: document.querySelector(DOMstrings.inputType).value,
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
--- budget.js
+++ budget.js
@@ -1,7 +1,20 @@
 $(document).ready(function () {
 
     $("#download-btn").click(function () {
-        alert("Download Button Clicked!")
+        
+        let transactions = getTransactions();
+
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:8083/budget-app/save",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(transactions),
+            success: function(result){
+                console.log(result);
+            },
+            dataType:"text"
+          });
+ 
     });
 
     $("#log-in").click(function () {
@@ -9,3 +22,43 @@ $(document).ready(function () {
     });
 
 });
+
+function getTransactions() {
+    let tnxList = [];
+    $('.income__list').children('div').each(function () {
+        let id = $(this).attr('id');
+        if (id.startsWith("inc-")) {
+            let tnx = createTransaction(id);
+            tnxList.push(tnx);
+        }
+    });
+    $('.expenses__list').children('div').each(function () {
+        let id = $(this).attr('id');
+        if (id.startsWith("exp-")) {
+            let tnx = createTransaction(id);
+            tnxList.push(tnx);
+        }
+    });
+    return tnxList;
+}
+
+function createTransaction(id) {
+        let type = getType(id);
+        let name = $("#" + id).find('.item__description').html();
+        let value = $("#" + id).find('.item__value').html();
+        return new Transaction(type, name, value);
+}
+
+function getType(id) {
+    if (id.startsWith("inc-")) {
+        return "income";
+    } else if (id.startsWith("exp-")) {
+        return "expense";
+    }
+}
+
+function Transaction(type, name, value) {
+    this.type = type;
+    this.name = name;
+    this.value = value;
+}
